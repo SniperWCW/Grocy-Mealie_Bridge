07aeb66da1b98c295177b8413fb96ff4211d65e1
Steffen <99438572+SniperWCW@users.noreply.github.com>
2026-05-15 08:56:00 +0200

    Create const.py for integration constants
    Added configuration constants for Mealie Grocy Bridge integration.

---
+++ custom_components/mealie_grocy_bridge/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Mealie Grocy Bridge integration."""
+
+DOMAIN = "mealie_grocy_bridge"
+
+# Konfigurations-Keys
+CONF_MEALIE_URL = "mealie_url"
+CONF_MEALIE_TOKEN = "mealie_token"
+CONF_GROCY_URL = "grocy_url"
+CONF_GROCY_TOKEN = "grocy_token"
